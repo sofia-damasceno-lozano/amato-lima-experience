04ea6fd93d5eb4bb2ba1767a87203b57e35fe29c
Sofia Lozano Damasceno <sofia.damasceno.lozano@gmail.com>
2026-05-19 12:15:02 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+
+  basePath: "/amato-lima-experience",
+  assetPrefix: "/amato-lima-experience/",
+
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
