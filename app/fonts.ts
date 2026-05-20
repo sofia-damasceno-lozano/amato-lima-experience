3dcb9b587f453f7e2772eb20cef21798341a5502
Sofia Lozano Damasceno <sofia.damasceno.lozano@gmail.com>
2026-05-19 22:03:21 -0300

    Create fonts.ts

---
+++ app/fonts.ts
@@ -0,0 +1,9 @@
+import localFont from "next/font/local";
+
+export const classyVogue = localFont({
+  src: "../public/fonts/classy-vogue.otf",
+
+  variable: "--font-classy-vogue",
+
+  display: "swap",
+});
