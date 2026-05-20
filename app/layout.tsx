4013667adc89fd835f606dc32e6c476bb5f30560
Sofia Lozano Damasceno <sofia.damasceno.lozano@gmail.com>
2026-05-19 22:03:54 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { classyVogue } from "./fonts";
 
 export default function RootLayout({
   children,
@@ -8,6 +9,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body>{children}</body>
+      <body className={classyVogue.variable}>
     </html>
   );
 }
