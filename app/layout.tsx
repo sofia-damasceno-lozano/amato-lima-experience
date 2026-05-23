eb7643bad1ae73fa893d3485e6959adb0c6c046c
Sofia Lozano Damasceno <sofia.damasceno.lozano@gmail.com>
2026-05-23 16:40:48 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,29 @@
+import type { Metadata } from "next";
 import "./globals.css";
-import { classyVogue, westwood } from "./fonts";
+
+import {
+  classyVogue,
+  westwoodStudio,
+} from "./fonts";
+
+export const metadata: Metadata = {
+  title: "Amato Lima",
+  description: "Ativos Imobiliários",
+};
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="pt-BR">
-      <body className={`${classyVogue.variable} ${westwood.variable}`}>
+      <body
+        className={`
+          ${classyVogue.variable}
+          ${westwoodStudio.variable}
+        `}
+      >
         {children}
       </body>
     </html>
