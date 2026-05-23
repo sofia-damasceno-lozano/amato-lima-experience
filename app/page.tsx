5e5a37c604c96f5f21b95883b09bd33653f41831
Sofia Lozano Damasceno <sofia.damasceno.lozano@gmail.com>
2026-05-23 14:32:02 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import HeroHome from "../components/hero-home/hero-home";
-import { EditorialAssetsSection } from "../components/home-after-hero/editorial-assets-section/editorial-assets-section";
 
 export default function Home() {
-  return (
-    <>
-      <HeroHome />
-      <EditorialAssetsSection />
-    </>
-  );
+  return <HeroHome />;
 }
