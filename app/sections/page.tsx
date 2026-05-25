b7cb94d40530577d15d47f6bb06c825cb5e3e3b2
Sofia Lozano Damasceno <sofia.damasceno.lozano@gmail.com>
2026-05-25 13:49:07 -0300

    Delete app/sections directory

--- app/sections/page.tsx
+++
@@ -1,5 +0,0 @@
-import SectionsHomeAfterHero from "../../components/sections-home-after-hero/sections-home-after-hero";
-
-export default function SectionsPage() {
-  return <SectionsHomeAfterHero />;
-}
