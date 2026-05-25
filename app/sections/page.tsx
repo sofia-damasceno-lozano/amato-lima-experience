06c04a73a0a609b76e764096b936110036b9c99f
Sofia Lozano Damasceno <sofia.damasceno.lozano@gmail.com>
2026-05-25 12:09:23 -0300

    Create page.tsx

---
+++ app/sections/page.tsx
@@ -0,0 +1,5 @@
+import SectionsHomeAfterHero from "../../components/sections-home-after-hero/sections-home-after-hero";
+
+export default function SectionsPage() {
+  return <SectionsHomeAfterHero />;
+}
