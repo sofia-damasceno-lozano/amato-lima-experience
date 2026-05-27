be506f22f479c2f0a3096dc7b2d0f7efacc6080c
Sofia Lozano Damasceno <sofia.damasceno.lozano@gmail.com>
2026-05-26 22:36:21 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import IntroHome from "../components/intro-home/intro-home";
 import HeroHome from "../components/hero-home/hero-home";
+import SectionsHomeAfterHero from "../components/sections-home-after-hero/sections-home-after-hero";
 
 export default function Home() {
   return (
     <>
       <IntroHome />
       <HeroHome />
+      <SectionsHomeAfterHero />
     </>
   );
 }
