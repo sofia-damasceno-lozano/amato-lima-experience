ec8953f0b6f5152bfc03d7002f3c77d72a2abb3c
Sofia Lozano Damasceno <sofia.damasceno.lozano@gmail.com>
2026-05-29 09:31:18 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import IntroHome from "../components/intro-home/intro-home";
 import HeroHome from "../components/hero-home/hero-home";
+import VisualImmersion from "../components/visual-immersion/visual-immersion";
 
 export default function Home() {
   return (
     <>
       <IntroHome />
       <HeroHome />
+      <VisualImmersion />
     </>
   );
 }
